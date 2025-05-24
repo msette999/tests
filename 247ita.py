022d86316725b264326ac498a8e6312f74f0385e
Marco Francesco Sette <95929175+msette999@users.noreply.github.com>
2025-05-24 18:58:57 +0200

    Update 247ita.py

--- 247ita.py
+++ 247ita.py
@@ -5,8 +5,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://msette999-test.hf.space"     # non mettere lo / finale al link
+MFPPSW = "qD5MyE@o9P"
 
 # Costanti
 M3U8_OUTPUT_FILE = "247ita.m3u8"
