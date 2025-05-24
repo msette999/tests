fda0dbbae473bd67b3a30e292a2d8be0e4e582b6
Marco Francesco Sette <95929175+msette999@users.noreply.github.com>
2025-05-24 19:01:50 +0200

    Update onlyevents.py

--- onlyevents.py
+++ onlyevents.py
@@ -9,8 +9,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://msette999-test.hf.space"     # non mettere lo / finale al link
+MFPPSW = "qD5MyE@o9P"
 
 # Costanti
 NUM_CHANNELS = 10000
